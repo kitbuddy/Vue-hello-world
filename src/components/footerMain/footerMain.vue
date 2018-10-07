228ec675a159c7af84a7e0fd74881e9c3f0e579f
ankit jain <me.ankit88@gmail.com>
2018-10-07 03:38:09 -0400

    add mask and message to button

---
+++ src/components/footerMain/footerMain.vue
@@ -0,0 +1,35 @@
+<template>
+
+  <section class="footerMain">
+    <div>
+     this is headerMain vue component
+    </div>
+  </section>
+
+</template>
+
+<script>
+import Vue from "vue";
+
+Vue.component("footerMain", {
+  // ... options ...
+});
+export default {
+  name: "footerMain",
+  props: [],
+  mounted() {},
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {}
+};
+</script>
+
+<style scoped>
+.footerMain {
+  height: 50px;
+  background-color: black;
+  margin-bottom: 0%
+}
+</style>
